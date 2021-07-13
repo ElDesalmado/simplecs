4480d0af758fdd7e1baa236b8573b6edaae70228
s.kolesnik <7fosGbc9S>
2021-07-13 19:23:22 +0500

    added stub tests for c_api

---
+++ test/test_c_api.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "simplesc/c_registry_api.h"
+
+#include <gtest/gtest.h>
+
+// TODO: make tests
+
+TEST(c_api_registry, reg_components)
+{
+    // TODO: implement
+
+    FAIL();
+}
+
+TEST(c_api_registry, unreg_components)
+{
+    // TODO: implement
+
+    FAIL();
+}
+
+TEST(c_api_registry, selec_entities)
+{
+    // TODO: implement
+
+    FAIL();
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
