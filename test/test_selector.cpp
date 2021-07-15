8754649e5fc0aa0de05f259e2523dc529876b54b
Dmitry Perminov <d.perminov@uwca.ru>
2021-07-15 12:15:58 +0500

    renamed from inputType to SelectInputType and added std::make_pair in insert for table

--- test/test_selector.cpp
+++ test/test_selector.cpp
@@ -10,21 +10,9 @@ using ComponentId = size_t;
 
 using SelectorStd = eld::generic::selector<eld::impl::selector_std<EntityId, ComponentId>>;
 
-using inputType = std::vector<EntityId>;
-using selectedType = std::vector<ComponentId>;
-using tableType = std::unordered_map<ComponentId, std::vector<EntityId>>;
-
-namespace
-{
-    unsigned long generateRandom(unsigned long lower_bound, unsigned long upper_bound)
-    {
-        std::uniform_int_distribution<unsigned long> uniformIntDistribution(lower_bound,
-                                                                            upper_bound);
-        std::random_device r;
-        std::default_random_engine re(r());
-        return uniformIntDistribution(re);
-    }
-}   // namespace
+using SelectInputType = std::vector<ComponentId>;
+using SelectedType = std::vector<ComponentId>;
+using TableType = std::unordered_map<ComponentId, std::vector<EntityId>>;
 
 class TestSelectorStd : public testing::Test
 {
@@ -32,26 +20,26 @@ protected:
     void SetUp() override { Test::SetUp(); }
     void TearDown() override { Test::TearDown(); }
     SelectorStd selector{};
-    tableType table{};
-    inputType input{};
-    selectedType selected{};
+    TableType table{};
+    SelectInputType inputComponents{};
+    SelectedType selected{};
 };
 // TODO: test scenarios and implementations
 
 TEST_F(TestSelectorStd, empty_table)
 {
-    input = { 4, 8, 15, 16, 23, 42 };
+    inputComponents = { 4, 8, 15, 16, 23, 42 };
 
-    selected = selector(table, input);
+    selected = selector(table, inputComponents);
     EXPECT_TRUE(selected.empty());
 }
 
 TEST_F(TestSelectorStd, empty_select_input)
 {
-    table.insert({ 0, { 4, 8, 15 } });
-    table.insert({ 1, { 16, 23, 42 } });
+    table.insert(std::make_pair(ComponentId(0), std::vector<EntityId>{ 4, 8, 15 }));
+    table.insert(std::make_pair(ComponentId(1), std::vector<EntityId>{ 16, 23, 42 }));
 
-    selected = selector(table, input);
+    selected = selector(table, inputComponents);
     EXPECT_TRUE(selected.empty());
 }
 
@@ -61,38 +49,43 @@ TEST_F(TestSelectorStd, single_column)
     std::iota(Ids.begin(), Ids.end(), 0);
     std::shuffle(Ids.begin(), Ids.end(), std::mt19937{ std::random_device{}() });
 
-    table.insert({ 0, Ids });
-    input.push_back(0);
-    selected = selector(table, input);
+    table.insert(std::make_pair(ComponentId(0), Ids));
+    inputComponents.push_back(0);
+    selected = selector(table, inputComponents);
+    std::sort(Ids.begin(), Ids.end());
     EXPECT_EQ(selected, Ids);
 }
 
-TEST_F(TestSelectorStd, single_column_empty_result_positive)
+TEST_F(TestSelectorStd, single_column_empty_result_positive_values)
 {
     std::vector<EntityId> Ids(100);
     std::iota(Ids.begin(), Ids.end(), 0);
     std::shuffle(Ids.begin(), Ids.end(), std::mt19937{ std::random_device{}() });
 
-    table.insert({ 0, Ids });
-    input = std::vector<ComponentId>(1000);
-    std::iota(input.begin(), input.end(), 1);
-    std::shuffle(input.begin(), input.end(), std::mt19937{ std::random_device{}() });
-    selected = selector(table, input);
-    EXPECT_EQ(selected, selectedType{});
+    table.insert(std::make_pair(ComponentId(0), Ids));
+    inputComponents = std::vector<ComponentId>(1000);
+    std::iota(inputComponents.begin(), inputComponents.end(), 1);
+    std::shuffle(inputComponents.begin(),
+                 inputComponents.end(),
+                 std::mt19937{ std::random_device{}() });
+    selected = selector(table, inputComponents);
+    EXPECT_EQ(selected, SelectedType{});
 }
 
-TEST_F(TestSelectorStd, single_column_empty_result_negative)
+TEST_F(TestSelectorStd, single_column_empty_result_negative_values)
 {
     std::vector<EntityId> Ids(100);
     std::iota(Ids.begin(), Ids.end(), 0);
     std::shuffle(Ids.begin(), Ids.end(), std::mt19937{ std::random_device{}() });
 
-    table.insert({ 0, Ids });
-    input = std::vector<ComponentId>(1000);
-    std::iota(input.begin(), input.end(), -1000);
-    std::shuffle(input.begin(), input.end(), std::mt19937{ std::random_device{}() });
-    selected = selector(table, input);
-    EXPECT_EQ(selected, selectedType{});
+    table.insert(std::make_pair(ComponentId(0), Ids));
+    inputComponents = std::vector<ComponentId>(1000);
+    std::iota(inputComponents.begin(), inputComponents.end(), -1000);
+    std::shuffle(inputComponents.begin(),
+                 inputComponents.end(),
+                 std::mt19937{ std::random_device{}() });
+    selected = selector(table, inputComponents);
+    EXPECT_EQ(selected, SelectedType{});
 }
 
 TEST_F(TestSelectorStd, thousand_columns_equal_entt_ids)
@@ -101,61 +94,88 @@ TEST_F(TestSelectorStd, thousand_columns_equal_entt_ids)
     std::iota(Ids.begin(), Ids.end(), 0);
     std::shuffle(Ids.begin(), Ids.end(), std::mt19937{ std::random_device{}() });
     for (size_t i = 0; i < 1000; ++i)
-        table.insert({ i, Ids });
+        table.insert(std::make_pair(ComponentId(i), Ids));
 
-    input = inputType(1000);
-    std::iota(input.begin(), input.end(), 0);
-    std::shuffle(input.begin(), input.end(), std::mt19937{ std::random_device{}() });
-    selected = selector(table, input);
-
+    inputComponents = SelectInputType(1000);
+    std::iota(inputComponents.begin(), inputComponents.end(), 0);
+    std::shuffle(inputComponents.begin(),
+                 inputComponents.end(),
+                 std::mt19937{ std::random_device{}() });
+    selected = selector(table, inputComponents);
+    std::sort(Ids.begin(), Ids.end());
     EXPECT_EQ(selected, Ids);
 }
 
 TEST_F(TestSelectorStd, three_columns_not_equal_entt_ids)
 {
-    table.insert({ 0, { 0, 1, 2, 3, 4, 5 } });
-    table.insert({ 1, { 2, 3, 0, 1, 4, 5 } });
-    table.insert({ 2, { 4, 5, 2, 3, 0, 1 } });
+    table.insert(std::make_pair(ComponentId(0), std::vector<EntityId>{ 0, 1, 2, 3, 4, 5 }));
+    table.insert(std::make_pair(ComponentId(1), std::vector<EntityId>{ 2, 3, 0, 1, 4, 5 }));
+    table.insert(std::make_pair(ComponentId(2), std::vector<EntityId>{ 4, 5, 2, 3, 0, 1 }));
 
-    input = { 0, 1, 2 };
-    selected = selector(table, input);
+    inputComponents = { 0, 1, 2 };
+    selected = selector(table, inputComponents);
     std::sort(selected.begin(), selected.end());
-    EXPECT_EQ(selected, (selectedType{ 0, 1, 2, 3, 4, 5 }));
+    EXPECT_EQ(selected, (SelectedType{ 0, 1, 2, 3, 4, 5 }));
 }
 
 TEST_F(TestSelectorStd, three_columns_extra_numbers)
 {
-    table.insert({ 0, { 0, 1, 2, 3, 4, 5 } });
-    table.insert({ 1, { 6, 7, 0, 1, 8, 9 } });
-    table.insert({ 2, { 10, 11, 12, 13, 0, 1 } });
+    table.insert(std::make_pair(ComponentId(0), std::vector<EntityId>{ 0, 1, 2, 3, 4, 5 }));
+    table.insert(std::make_pair(ComponentId(1), std::vector<EntityId>{ 6, 7, 0, 1, 8, 9 }));
+    table.insert(std::make_pair(ComponentId(2), std::vector<EntityId>{ 10, 11, 12, 13, 0, 1 }));
 
-    input = { 0, 1, 2 };
-    selected = selector(table, input);
+    inputComponents = { 0, 1, 2 };
+    selected = selector(table, inputComponents);
     std::sort(selected.begin(), selected.end());
-    EXPECT_EQ(selected, (selectedType{ 0, 1 }));
+    EXPECT_EQ(selected, (SelectedType{ 0, 1 }));
 }
 
 TEST_F(TestSelectorStd, three_columns_empty_ids)
 {
-    table.insert({ 3, {} });
-    table.insert({ 1, {} });
-    table.insert({ 2, {} });
+    table.insert(std::make_pair(ComponentId(3), std::vector<EntityId>{}));
+    table.insert(std::make_pair(ComponentId(1), std::vector<EntityId>{}));
+    table.insert(std::make_pair(ComponentId(2), std::vector<EntityId>{}));
 
-    input = { 0, 1, 2 };
-    selected = selector(table, input);
+    inputComponents = { 0, 1, 2 };
+    selected = selector(table, inputComponents);
     std::sort(selected.begin(), selected.end());
-    EXPECT_EQ(selected, selectedType{});
+    EXPECT_EQ(selected, SelectedType{});
 }
 
 TEST_F(TestSelectorStd, three_columns_empty_result)
 {
-    table.insert({ 0, { 1, 2, 3, 4, 5 } });
-    table.insert({ 1, { 2, 4, 6 } });
-    table.insert({ 42, {} });
-    input = { 0, 1, 42 };
-    selected = selector(table, input);
+    inputComponents = { 0, 1, 2 };
 
-    EXPECT_EQ(selected, selectedType{});
+    table.insert(std::make_pair(ComponentId(0), std::vector<EntityId>{ 1, 2, 3, 4, 5 }));
+    table.insert(std::make_pair(ComponentId(1), std::vector<EntityId>{ 2, 4, 6 }));
+    table.insert(std::make_pair(ComponentId(2), std::vector<EntityId>{}));
+
+    selected = selector(table, inputComponents);
+    EXPECT_EQ(selected, SelectedType{});
+
+    table.clear();
+    table.insert(std::make_pair(ComponentId(0), std::vector<EntityId>{ 1, 2, 3, 4, 5 }));
+    table.insert(std::make_pair(ComponentId(1), std::vector<EntityId>{}));
+    table.insert(std::make_pair(ComponentId(2), std::vector<EntityId>{ 2, 4, 6 }));
+
+    selected = selector(table, inputComponents);
+    EXPECT_EQ(selector(table, inputComponents), SelectedType{});
+
+    table.clear();
+    table.insert(std::make_pair(ComponentId(0), std::vector<EntityId>{}));
+    table.insert(std::make_pair(ComponentId(1), std::vector<EntityId>{ 1, 2, 3, 4, 5 }));
+    table.insert(std::make_pair(ComponentId(2), std::vector<EntityId>{ 2, 4, 6 }));
+
+    selected = selector(table, inputComponents);
+    EXPECT_EQ(selector(table, inputComponents), SelectedType{});
+
+    table.clear();
+    table.insert(std::make_pair(ComponentId(0), std::vector<EntityId>{ 45, 46, 101 }));
+    table.insert(std::make_pair(ComponentId(1), std::vector<EntityId>{ 1, 2, 3, 4, 5 }));
+    table.insert(std::make_pair(ComponentId(2), std::vector<EntityId>{ 2, 4, 6 }));
+
+    selected = selector(table, inputComponents);
+    EXPECT_EQ(selector(table, inputComponents), SelectedType{});
 }
 
 int main(int argc, char **argv)
